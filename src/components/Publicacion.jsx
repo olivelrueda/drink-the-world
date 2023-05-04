cca13552fea52245c11a5fe03d61e57bc817b1c1
Olivel Rueda <olivelrueda@gmail.com>
2023-05-04 16:48:38 +0200

    Paren el sufrimiento

--- src/components/Publicacion.jsx
+++ src/components/Publicacion.jsx
@@ -1,11 +1,16 @@
 import { apiURL } from "../config"
+import { useState } from "react"
+
 import useAuth from "../hooks/useAuth"
-import Stars from "./Stars"
+import StartRanking from "./StartRanking"
 
 export default function Publicacion({viaje, timeAgo, styles}) {
     const { user } = useAuth()
+    const [votesCounter, setVotesCounter] = useState(viaje.map)
+
+
+
     return <li>
-    <Stars/> 
     <h2 className={styles.place}>{viaje.place}</h2>
     <h3 className={styles.date}>{timeAgo.format(new Date(viaje.date))}</h3>
     {viaje.photo && (
@@ -19,6 +24,7 @@ export default function Publicacion({viaje, timeAgo, styles}) {
     )}
     <p className={styles.description}>{viaje.description}</p>
     <h4 className={styles.votos}>{Number.parseInt(viaje.votes)}</h4>
+    <StartRanking/>
     {/* <p className={styles.space}>{user && user.id === viaje.user_id ? "Es tu viaje" : "No es su viaje"}</p> */}
-</li>    
+</li>
 }
