ccc7ef2a800c56a9a377f8b0335efdfff055ad44
Victor Leon <victorfencing24@gmail.com>
2023-05-05 10:57:03 +0200

    .

--- src/components/Publicacion.jsx
+++ src/components/Publicacion.jsx
@@ -1,16 +1,14 @@
 import { apiURL } from "../config"
 import useAuth from "../hooks/useAuth"
-import Stars from "./Stars"
 
 export default function Publicacion({viaje, timeAgo, styles}) {
     const { user } = useAuth()
     return <li>
-    <Stars/> 
     <h2 className={styles.place}>{viaje.place}</h2>
     <h3 className={styles.date}>{timeAgo.format(new Date(viaje.date))}</h3>
     {viaje.photo && (
     <div>
-        {viaje.photo.map((photo) => (
+        {(viaje.photo.map((photo) => 
         <div key={photo.id}>
             <img className={styles.img} src={`${apiURL}/${photo.photo}`} alt="imagen del viaje" />
         </div>
