afa3caa64fd919ae31af1c959a6166f66f17af21
Irene Vanosote <irenecvp93@gmail.com>
2023-05-16 22:32:29 +0200

    prueba pull

--- src/components/Publicacion.jsx
+++ src/components/Publicacion.jsx
@@ -1,9 +1,7 @@
 import { apiURL } from "../config"
 import useAuth from "../hooks/useAuth"
-
-<<<<<<< HEAD
-import useAuth from '../hooks/useAuth';
-import useServer from '../hooks/useServer';
+import useServer from "../hooks/useServer"
+import { useState } from "react"
 
 export default function Publicacion({ viaje, timeAgo, styles }) {
 
@@ -38,18 +36,6 @@ export default function Publicacion({ viaje, timeAgo, styles }) {
               />
             </div>
           ))}
-=======
-export default function Publicacion({viaje, timeAgo, styles}) {
-    const { user } = useAuth()
-    return <li>
-    <h2 className={styles.place}>{viaje.place}</h2>
-    <h3 className={styles.date}>{timeAgo.format(new Date(viaje.date))}</h3>
-    {viaje.photo && (
-    <div>
-        {(viaje.photo.map((photo) => 
-        <div key={photo.id}>
-            <img className={styles.img} src={`${apiURL}/${photo.photo}`} alt="imagen del viaje" />
->>>>>>> 9470acbc87cbab10f8620ba7b5cba9de96672b78
         </div>
       )}
       <p className={styles.description}>{viaje.description}</p>
