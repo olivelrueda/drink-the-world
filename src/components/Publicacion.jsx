a11cfd40c44ef12f7c284c22ee819d61495f531c
Olivel Rueda <olivelrueda@gmail.com>
2023-05-18 16:04:18 +0200

    Co-authored-by: scrocchi <scrocchi@gmail.com>

--- src/components/Publicacion.jsx
+++ src/components/Publicacion.jsx
@@ -1,7 +1,6 @@
 import { apiURL } from "../config"
 import useAuth from "../hooks/useAuth"
 
-<<<<<<< HEAD
 import useAuth from '../hooks/useAuth';
 import useServer from '../hooks/useServer';
 
@@ -38,18 +37,6 @@ export default function Publicacion({ viaje, timeAgo, styles }) {
               />
             </div>
           ))}
-=======
-export default function Publicacion({viaje, timeAgo, styles}) {
-    const { user } = useAuth()
-    return <li>
-    <h2 className={styles.place}>{viaje.place}</h2>
-    <h3 className={styles.date}>{timeAgo.format(new Date(viaje.date))}</h3>
-    {viaje.photo && (
-    <div>
-        {(viaje.photo.map((photo) => 
-        <div key={photo.id}>
-            <img className={styles.img} src={`${apiURL}/${photo.photo}`} alt="imagen del viaje" />
->>>>>>> 9470acbc87cbab10f8620ba7b5cba9de96672b78
         </div>
       )}
       <p className={styles.description}>{viaje.description}</p>
